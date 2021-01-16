038e6dcb9b395bea423e5bf2b6c2f811ca59d29e
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-15 21:48:19 -0500

    starting logic for adding the active class

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,5 +3,7 @@ const hearFace = document.querySelector(".hear")
 const speakFace = document.querySelector(".speak")
 
 seeFace.addEventListener("click", () => {
-    
+    if(seeFace.classList.contains("see")) {
+        
+    }
 })
