61905435e4b9fd48db89cae840562332c264ec17
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-15 21:40:12 -0500

    event listener

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,7 @@
 const seeFace = document.querySelector(".see")
 const hearFace = document.querySelector(".hear")
 const speakFace = document.querySelector(".speak")
+
+seeFace.addEventListener("click", () => {
+    
+})
